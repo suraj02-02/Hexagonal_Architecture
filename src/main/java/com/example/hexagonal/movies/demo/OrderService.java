d7cf2e0f3ab3f02cd0dcec6452f12818763a11fe
surajyadav <surajyadav020406@gmail.com>
2024-05-29 16:26:41 +0530

    This is a demo project based on Hexagonal Architecture

--- src/main/java/com/example/hexagonal/movies/demo/OrderService.java
+++ src/main/java/com/example/hexagonal/movies/demo/OrderService.java
@@ -9,10 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * For now we have excluded any usage of Datasource in our application since
  * it is just a demo application , and it's main use case is to learn the hexagonal architecture.
  *
- * The detail on the hexagonal architecture and how it is implemented in this project can be found at :
- *
- *
- *
+ * The detail on the hexagonal architecture and how it is implemented in this project can be found at : src/main/resources/static/Hexagonal_Architecture_Explained
  */
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
